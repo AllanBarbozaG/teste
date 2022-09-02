b742d0c5096a4ddb8b1c804602e7c4c7238b7d04
allanbarbozag <allpatrick.b@gmail.com>
2022-09-02 16:34:06 -0300

    .

--- src/Pages/index/index.js
+++ src/Pages/index/index.js
@@ -1,4 +1,11 @@
+const teste = document.querySelector('.teste')
+
+document.addEventListener('onload', (teste, () => {
+  console.log('oi')
+}))
+
 document.addEventListener('DOMContentLoaded', (e) => {
+  setNavbarHeightAndOpacity()
 
 })
 
@@ -6,68 +13,30 @@ document.addEventListener('click', (e) => {
   console.log(e)
 })
 
+function setNavbarHeightAndOpacity() {
+  const navbar = document.querySelector('.navbar-style')
+  const main = document.querySelector('#main')
+  const teste = document.querySelector('.teste')
 
+  navbar.addEventListener('mouseover', () => {
+    if (navbar.classList.contains('hide')) {
+      navbar.classList.add('show')
+      navbar.classList.remove('hide')
+    }
+  })
 
+  main.addEventListener('mouseover', () => {
+    if (navbar.classList.contains('hide')) {
+      navbar.classList.add('show')
+      navbar.classList.remove('hide')
+    }
+  })
 
+  teste.addEventListener('mouseover', () => {
+    if (navbar.classList.contains('show')) {
+      navbar.classList.add('hide');
+      navbar.classList.remove('show')
+    }
+  })
+}
 
-
-
-// function setNavbarHeight() {
-//   const main = document.querySelector('#main');
-//   const navbar = document.querySelector('.navbar-style');
-//   const teste = document.querySelector('.teste');
-
-//   main.addEventListener('mouseover', () => {
-//     navbar.style.height = '7rem';   
-//     navbar.style.opacity = '1'    
-//   })
-
-//   teste.addEventListener('mouseover' , () => {
-//     navbar.style.height = '3rem';   
-//     navbar.style.opacity = '0.8'
-//   })  
-
-//   navbar.addEventListener('mouseover', () => {
-//     navbar.style.height = '7rem';   
-//     navbar.style.opacity = '1';
-//   })
-
-//   navbar.addEventListener('mouseout', () => {
-//     navbar.style.height = '3rem';   
-//     navbar.style.opacity = '0.8';
-//   })
-
-
-
-//   // teste.addEventListener('mouseout', () => {    
-//   //   navbar.style.height = '7rem';
-//   // })
-// }
-// setNavbarHeight();
-
-const navbar = document.querySelector('.navbar-style')
-const main = document.querySelector('#main')
-const teste = document.querySelector('.teste')
-
-
-
-navbar.addEventListener('mouseover', () => {
-  if (navbar.classList.contains('hide')) {
-    navbar.classList.add('show')
-    navbar.classList.remove('hide')
-  }
-})
-
-main.addEventListener('mouseover', () => {
-  if (navbar.classList.contains('hide')) {
-    navbar.classList.add('show')
-    navbar.classList.remove('hide')
-  }
-})
-
-teste.addEventListener('mouseover', () => {
-  if (navbar.classList.contains('show')) {
-    navbar.classList.add('hide');
-    navbar.classList.remove('show')
-  }
-})
